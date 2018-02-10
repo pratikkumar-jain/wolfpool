b048b813fdcf44dc07700f52f0a029bb72649385
rjain10 <rjain10@ncsu.edu>
2018-02-10 16:43:13 -0500

    Few minor changes

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.get('/about', function(req, res){
   res.render('about');
 });
 
+app.get('/create_plan_page', function(req, res){
+  res.render('create_plan_page');
+});
+
 app.get('/contact', function(req, res){
   res.render('contact', { csrf: 'CSRF token here'});
 });
