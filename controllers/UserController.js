8f9e0878258a0efcef5d026f055e99e2a4de4e8c
Chirag Jain <csjain@ncsu.edu>
2018-02-21 22:29:22 -0500

    Pivoting on the email serivce provider, code working on local checking on AWS

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,6 @@ var schedule = require('node-schedule');
 exports.createUser = function(req, res){
 
   var User = require('../models/user');
-  var nodemailer = require('nodemailer');
-  var aws = require('aws-sdk');
   var md5 = require('md5');
 
   // Check if all parameters are passed
@@ -29,37 +27,32 @@ exports.createUser = function(req, res){
           return res.render('500');
         } else {
 
-          // Creating Transporter using AWS SES
-          // var transporter = nodemailer.createTransport(ses({
-          //     accessKeyId: 'AKIAJCUM65Y7TBJXYGFQ',
-          //     secretAccessKey: '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'
-          // }));
-
-          // configure AWS SDK
-          aws.config.loadFromPath('config.json');
-
-          let transporter = nodemailer.createTransport({
-              SES: new aws.SES({
-                  apiVersion: '2010-12-01'
-              })
-          });
-
           var websitehost = 'http://localhost:3000';
-          var mailOptions = {
-            from: 'sengncsu2018@gmail.com',
-            to: req.body.email,
-            subject: 'Wolfpool user verification',
-            html: '<h1>Please click on the <a href="' + websitehost + '/verify_user/' + req.body.email + '/' + verfhash + '">link</a> to verify your account</h1>The link will expire in 24 hours'
-          };
+          var websitehost = 'http://localhost:3000';
+          
+          var mailjet = require('node-mailjet').connect('c085fa507e095331306f61731ed41479', '9100f833f4a236f708e99a9a00be60fb')
+          var request = mailjet
+              .post("send")
+              .request({
+                  "FromEmail":"sengncsu2018@gmail.com",
+                  "FromName":"Wolfpool Support",
+                  "Subject":'Wolfpool user verification',
+                  "Html-part":'<h3>Please click on the <a href="' + websitehost + '/verify_user/' + req.body.email + '/' + verfhash + '">link</a> to verify your account</h3>The link will expire in 24 hours',
+                  "Recipients":[
+                          {
+                                  "Email": req.body.email
+                          }
+                  ]
+              });
 
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
+          request
+          .then((result) => {
+              console.log(result);
               return res.render('info_page',{data:'An email has been sent to you with verification link.'});
-            }
-          });
+          })
+          .catch((err) => {
+              console.log(err.statusCode)
+          })
         }
       });
 
@@ -138,5 +131,5 @@ var j = schedule.scheduleJob(rule, function(){
   console.log('Batch Executed');
   var Users = require('../models/user');
   var query={"verified":false};  //add check for date>=24 hrs in past
-  Users.find(query).remove().exec();
+  //Users.find(query).remove().exec();
 });
