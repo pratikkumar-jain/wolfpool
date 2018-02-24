6c285a316012b646684cc7f64bb7a904a80830a0
Pratik Jain <pjain22@ncsu.edu>
2018-02-24 03:21:55 -0500

    No Logout without Login

--- router.js
+++ router.js
@@ -6,75 +6,107 @@ var PlanController = require('./controllers/PlanController');
 // Routes
 module.exports = function(app){
 	app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(function(req, res, next) {
+		res.locals.user = req.session.userId;
+		next();
+	});
 
 	// General routes
-  app.get('/', function(req, res){
-  		res.render('login');
+	app.get('/', function(req, res){
+			res.render('login');
 	});
-  app.get('/home', function(req,res){
-  		res.render('home')
-  });
+	// app.get('/home', function(req,res){
+	// 		res.render('home')
+	// });
 	app.get('/about', function(req, res){
-  		res.render('about');
+			res.render('about');
 	});
 
 	// Routes related to Plan
-	app.get('/create_search_plan_page', function(req, res){
+	app.get('/home', function(req, res){
 		if (req.session && req.session.userId) {
-			res.render('create_search_plan_page');
+			res.render('home');
 		} else {
 			res.render('info_page',{data: 'You must be logged in to view this page. Back to ', name:'login', link:'login_page'});
 		}
 	});
-  app.post('/savePlan',PlanController.savePlan);
-  app.get('/search_plan_page', function(req, res){
+	app.get('/profile_page', function(req, res){
+		if (req.session && req.session.userId) {
+			res.render('profile_page');
+		} else {
+			res.render('info_page',{data: 'You must be logged in to view this page. Back to ', name:'login', link:'login_page'});
+		}
+	});
+	// app.post('/savePlan',PlanController.savePlan);
+	app.get('/search_plan_page', function(req, res){
 		res.render('search_plan_page');
 	});
-
-
-	app.post('/searchPlan',PlanController.searchPlan);
-	app.post('/joinPlan',PlanController.joinPlan);
-	// app.post('/searchPlan', function(req, res){
-  //
-	// 	PlanController.searchPlan(req, res);
-  //
-	// 	//if (err) return res.sendStatus(500);
-  //
-  //
-	// 	console.log("inside search plan");
-	// 	//res.render('contact', { csrf: 'CSRF token here'});
-	// });
-
-  app.get('/contact', function(req, res){
+	app.get('/contact', function(req, res){
 		res.render('contact', { csrf: 'CSRF token here'});
 	});
 	app.get('/search_plan_page', function(req, res){
 		res.render('search_plan_page');
 	});
-
-	// Routes related to User
 	app.get('/profile_page', function(req, res){
 		res.render('profile_page');
 		//res.render('profile_page',{success:false, errors: req.session.errors});
 		//req.session.errors=null;
 	});
-	app.post('/profile_page', function(req, res){
-		res.redirect('/profile_page');
-	});
 	app.get('/register_page', function(req, res){
-			res.render('register');
+		res.render('register');
 	});
 	app.get('/verify_user/:email/:verfhash', UserController.verifyUser);
-	app.post('/createUser', UserController.createUser);
+	// app.post('/createUser', UserController.createUser);
 	app.get('/login_page', function(req, res){
-			res.render('login');
+		res.render('login');
 	});
-	app.post('/loginUser', UserController.loginUser);
+	// app.post('/loginUser', UserController.loginUser);
 	app.get('/logout_page', UserController.logoutUser);
 
 	app.get('/info_page', function(req, res){
 		res.render('info_page',{data: 'Welcome. Click here to  ', name:'login', link:'login_page'});
 	});
 
-};
+	app.post('/savePlan',PlanController.savePlan);
+	app.post('/searchPlan',PlanController.searchPlan);
+	app.post('/joinPlan',PlanController.joinPlan);
+	app.post('/profile_page', function(req, res){
+		res.redirect('/profile_page');
+	});
+	app.post('/createUser', UserController.createUser);
+	app.post('/loginUser', UserController.loginUser);
+	// app.post('/searchPlan', function(req, res){
+	//
+	// 	PlanController.searchPlan(req, res);
+	//
+	// 	//if (err) return res.sendStatus(500);
+	//
+	//
+	// 	console.log("inside search plan");
+	// 	//res.render('contact', { csrf: 'CSRF token here'});
+	// });
+	// app.post('/profile_page', function(req, res){
+	// 	res.redirect('/profile_page');
+	// });
+	
+
+	// Routes related to User
+	
+	
+	// app.get('/register_page', function(req, res){
+	// 		res.render('register');
+	// });
+	// app.get('/verify_user/:email/:verfhash', UserController.verifyUser);
+	// // app.post('/createUser', UserController.createUser);
+	// app.get('/login_page', function(req, res){
+	// 		res.render('login');
+	// });
+	// // app.post('/loginUser', UserController.loginUser);
+	// app.get('/logout_page', UserController.logoutUser);
+
+	// app.get('/info_page', function(req, res){
+	// 	res.render('info_page',{data: 'Welcome. Click here to  ', name:'login', link:'login_page'});
+	// });
+
+	};
